dd30ddb2148010bedc6a25449b5d2d74b1f78802
sergebezborodov <mail@sergebezborodov.com>
2013-02-16 22:54:49 +0200

    sphinx query

---
+++ ESphinxQuery.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Class implements sphinx query model.
+ * Query contains search text, indexes list, and sphinx criteria.
+ */
+class ESphinxQuery extends CComponent
+{
+	/**
+	 * @var string
+	 */
+	private $_text;
+
+	/**
+	 * @var string
+	 */
+	private $_indexes;
+
+	/**
+	 * @var ESphinxSearchCriteria
+	 */
+	private $_criteria;
+
+	/**
+	 * Query constructor.
+	 * 
+	 * @param string $text search phrase
+	 * @param string $indexes list of indexes
+	 * @param ESphinxCriteria $criteria search criterias
+	 */
+	public function __construct($text, $indexes="*", $criteria = null)
+	{
+		$this->_text = (string)$text;
+
+		if ($criteria instanceof ESphinxCriteria) {
+	        $this->_criteria = $criteria;
+        } else if(is_array($criteria)) {
+			$this->_criteria = new ESphinxCriteria($criteria);
+        } else {
+            $this->_criteria = new ESphinxCriteria();
+        }
+
+	    if (is_array($indexes)) {
+            $this->_indexes = implode(" ", $indexes);
+        } else {
+            $this->_indexes = (string)$indexes;
+        }
+	}
+
+	/**
+	 * Get search query
+     *
+	 * @return string
+	 */
+	public function getText()
+	{
+		return $this->_text;
+	}
+
+	/**
+	 * Get list indexes as string
+     *
+	 * @return string
+	 */
+	public function getIndexes()
+	{
+		return $this->_indexes;
+	}
+
+	/**
+	 * Get search criteria
+     *
+	 * @return ESphinxCriteria
+	 */
+	public function getCriteria()
+	{
+		return $this->_criteria;
+	}
+}
